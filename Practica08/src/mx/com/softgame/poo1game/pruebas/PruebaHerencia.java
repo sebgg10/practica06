85f5a5e67e2a96c58df4ca0fa6723ede6ff6b0e4
sebgg10 <79207578+sebgg10@users.noreply.github.com>
2021-03-05 10:42:49 -0600

    Inicio PruebaHerencia
    Se creó el archivo PruebaHerencia y se agregó el método principal y se crearon 2 personajes

---
+++ Practica08/src/mx/com/softgame/poo1game/pruebas/PruebaHerencia.java
@@ -0,0 +1,13 @@
+package max.com.softgame.poo1game.pruebas;
+import mx.com.softgame.poo1game.personajes.Personaje;
+import mx.com.softgame.poo1game.personajes.buenos.Planta;
+import mx.com.softgame.poo1game.personajes.malos.Zombie;
+
+public class PruebaHerencia {
+	public static void main(String[] args){
+		Personaje per01 = new Personaje("Valeria",99);
+		Personaje per02 = new Personaje("Octavio");
+		
+		
+	}
+}
